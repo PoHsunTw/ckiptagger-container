0a3523a9aca60b7ebb6f562b6cb0fc7bc02b7bfb
Po-Hsun Chen <hsun0955@gmail.com>
2019-12-31 17:37:18 +0800

    update

--- preDownload.py
+++ preDownload.py
@@ -1,5 +1,10 @@
-from ckiptagger import data_utils, construct_dictionary, WS, POS, NER
+from ckiptagger import data_utils
+import sys
 
 def main():
     # Download data
     data_utils.download_data("./")
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
